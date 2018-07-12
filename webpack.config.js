83f07597a11de6835b43f42174edceaa583ee400
Tejas Bubane <tejasbubane@gmail.com>
2018-07-12 22:43:33 +0530

    Do not compile js in source directory
    Let src be in reason only - less clutter.

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const outputDir = path.join(__dirname, "dist/");
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  entry: './src/Index.bs.js',
+  entry: './lib/js/src/Index.bs.js',
   mode: isProd ? 'production' : 'development',
   output: {
     path: outputDir,
